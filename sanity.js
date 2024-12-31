a7bf3e4de0fbe9b47356bd2c6d28bcdd773c6235
1stcodekami <mkkhurshid33@gmail.com>
2024-12-31 22:10:41 +0500

    Milestone 3 Blog

--- sanity.js
+++ sanity.js
@@ -3,7 +3,7 @@ import createImageUrlBuilder from "@sanity/image-url"
  
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion =process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+const apiVersion =process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-12-28";
 
 export const config = {
     projectId,
